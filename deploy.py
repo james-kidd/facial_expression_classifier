c3d25f7d039b490ae32386dd48275c491f769864
James Kidd <james.kidd02@hotmail.com>
2022-12-03 01:16:19 -0500

    deployment model made

---
+++ deploy.py
@@ -0,0 +1,38 @@
+
+import os 
+from flask import Flask, render_template, request
+from functions.showcase_prediction import predict_image, showcase_prediction
+from keras.models import load_model
+
+
+
+app = Flask(__name__)
+
+model = load_model('my_model_1')
+
+picFolder = os.path.join('static', 'pics')
+app.config['UPLOAD_FOLDER'] = picFolder
+
+
+@app.route('/', methods=["GET"])
+def home():
+    return render_template('home.html')
+
+
+@app.route('/', methods=["POST", "GET"])
+def predict():
+    imagefile = request.files['imagefile']     
+    
+    # used to upload the photo
+    image_path = os.path.join(app.config['UPLOAD_FOLDER'], imagefile.filename)
+    imagefile.save(image_path)                                          
+    
+    #generate predictions
+    predict = predict_image(image_path, model)
+    demo_img_path = showcase_prediction(predict, image_path)
+    
+    return render_template('home.html', img_path = demo_img_path )
+
+
+if __name__ =='__main__':
+	app.run(port = 4000, debug = True)
